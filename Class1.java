ce89f3a9569e1747bfb9984cbd7b48d0c2ce06ad
Srilekha9124 <113813435+Srilekha9124@users.noreply.github.com>
2022-09-19 13:55:06 +0530

    Add files via upload

---
+++ Class1.java
@@ -0,0 +1,27 @@
+package new_pkg;
+
+public class Class1 
+{
+	
+	public static void main(String[] args)
+	{
+		
+		//byte a = -128;
+		//byte b = 127;
+		byte  c  = (byte)(127+127) ;
+		System.out.println(c);
+		float f = 1.24f;
+		float  e = 1.24f;
+		double v = f+e;
+		System.out.println(v);
+		
+		int h = 1;
+		int j = 10;
+		float x = h+j;
+		
+		System.out.println(x);
+		
+	}
+		
+
+}
